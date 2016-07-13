f837557f285000bd43bec01cc25eaee8f5383a26
deetriplett <deetriplett@gmail.com>
2016-07-12 18:02:57 -0700

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+	</div>
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>
+				<li><a href="http://twitter.com/deetriplett">Twitter</a></li>
+				<li><a href="https://www.facebook.com/deetriplett">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date('Y'); ?> Eat Me Sugar</p>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
