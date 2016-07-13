575a19967b2303ebc221384e29df85f644b3c77a
deetriplett <deetriplett@gmail.com>
2016-07-12 18:04:14 -0700

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title><?php echo $pageTitle; ?></title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+</head>
+<body>
+
+	<div class="header">
+
+		<div class="wrapper">
+
+			<h1 class="branding-title"><a href="img/branding-title.png">Eat Me Sugar</a></h1>
+
+			<ul class="nav">
+				<li class="scrubs <?php if ($section == "scrubs") { echo "on"; } ?>"><a href="scrubs.php">Scrubs</a></li>
+				<li class="contact <?php if ($section == "contact") { echo "on"; } ?>"><a href="contact.php">Contact</a></li>
+				<!-- <li class="cart"><a target="paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_cart&amp;business=Q6NFNPFRBWR8S&amp;display=1">Shopping Cart</a></li> -->
+			</ul>
+
+		</div>
+
+	</div>
+
+	<div id="content">
