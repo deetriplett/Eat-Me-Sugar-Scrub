b39ff5be46d10c73c195c429b5e45d983fc28924
deetriplett <deetriplett@gmail.com>
2016-07-12 18:16:03 -0700

    Create scrubs.php

---
+++ scrubs.php
@@ -0,0 +1,23 @@
+<?php include("inc/products.php"); ?><?php
+$pageTitle = "Full Inventory of Sugar Scrub";
+$section = "scrubs";
+include('inc/header.php'); ?>
+
+		<div class="section Scrubs page">
+
+			<div class="wrapper">
+
+				<h1>Welcome to the Pantry</h1>
+
+				<ul class="products">
+					<?php foreach($products as $product_id => $product) {
+							echo get_list_view_html($product_id,$product);
+						}
+					?>
+				</ul>
+
+			</div>
+
+		</div>
+
+<?php include('inc/footer.php') ?>
