3c77609ba76917ed9667fb664fa4a3b39395ab8d
deetriplett <deetriplett@gmail.com>
2016-07-15 11:55:06 -0700

    added class container-fluid

--- scrub.php
+++ scrub.php
@@ -15,27 +15,29 @@ $section = "scrubs";
 $pageTitle = $product["name"];
 include("inc/header.php"); ?>
 
+<div class="container-fluid">
 		<div class="section page">
-
-			<div class="wrapper">
-
 				<div class="breadcrumb"><a href="scrubs.php">Sugar Scrubs</a> &gt; <?php echo $product["name"]; ?></div>
 
-				<div class="scrub-picture">
+			<div class="container-fluid">
+				<div class="col-md-4">
+				<div class="scrub-picture img-responsive">
 					<span>
-
-						<img src="<?php echo $product["img"]; ?>" alt="<?php echo $product["name"]; ?>">
-
+						<img src="<?php echo $product["img"]; ?>" class="img-responsive" alt="<?php echo $product["name"]; ?>">
 					</span>
 				</div>
-
-				<div class="scrub-details">
-
-					<h1><span class="price">$<?php echo $product["price"]; ?></span> <?php echo $product["name"]; ?></h1>
-
 				</div>
 
-			</div>
+
+				<div class="col-md-6">
+				<div class="scrub-details">
+					<h1><span class="price">$<?php echo $product["price"]; ?></span> <?php echo $product["name"]; ?></h1>
+					<p> <?php echo $product["description"]; ?> </p>
+				</div>
+				</div>
+				<div class="col-md-2"></div>
+		</div>
+	</div>
 
 		</div>
 
