98133b6ec6c4ab0b7c55772279f675b22785484a
deetriplett <deetriplett@gmail.com>
2016-07-15 11:53:01 -0700

    added bootstrap formatting

--- index.php
+++ index.php
@@ -2,21 +2,22 @@
 $pageTitle = "Eat Me Sugar Scrubs";
 $section = "home";
 include('inc/header.php'); ?>
-		<div class="section banner" id="content">
-			<div class="wrapper">
-						  <h2>So good you'll want to eat it, but you probably shouldn't. </h2>
+<div class="container-fluid jumbotron text-center">
+  <div id="content">
+						  <h1>So good you'll want to eat it, but you probably shouldn't. </h1>
+
 							<p>Sugar is not so good for your waist line, but it's terrific for your skin! Your body deserves the best cleansing and moisturizing products possible - and Eat Me Sugar's
 								handmade exfoliating body scrubs deliver the ultimate cleanse while restoring moisture and providing a deep cleanse that other soaps can't.</p>
 	            <p> With an unique formulation that provides a one of a kind clean, Eat Me Sugar scrubs leave your skin nourished without excess oil or dryness.</p>
-	          </a>
-			</div>
 		</div>
-
-		<div class="section scrubs">
+	</div>
+<div class="container-fluid text-center">
+	<div class="section scrubs">
 			<div class="wrapper">
 				<h2>Indulge yourself. Pick a flavor. </h2>
 
 				<?php include("inc/products.php"); ?>
+				<div class="container-fluid text-center">
 				<ul class="products">
 					<?php
 
@@ -32,9 +33,11 @@ include('inc/header.php'); ?>
 						echo $list_view_html;
 					?>
 				</ul>
-
+</div>
 			</div>
-
+</div>
 		</div>
 
+<div class="container-fluid">
 <?php include('inc/footer.php') ?>
+</div>
