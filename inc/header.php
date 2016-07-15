5c0cef4f838ba079199afc684b08e36d197d8e30
deetriplett <deetriplett@gmail.com>
2016-07-15 11:48:27 -0700

    add bootstrap

--- inc/header.php
+++ inc/header.php
@@ -1,32 +1,30 @@
 <html>
 <head>
 	<title><?php echo $pageTitle; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
 		integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<!-- <link rel="stylesheet" href="css/bootrap.css" type="text/css"> -->
 </head>
 <body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1>
-				<span class="branding-title">
-					<a href="index.php"><img src ="img/branding-title.png">
-					</a>
-				</span>
-			</h1>
-
-			<ul class="nav">
+<div class="container-fluid" >
+		<nav class="navbar navbar-inverse">
+			<div class="header">
+						<span class="branding-title ">
+							<a href="index.php"><img src ="img/branding-title.png" class="img-responsive" alt="Eat Me Sugar Scrubs">
+							</a>
+						</span>
+				<div class="container-fluid">
+ 				<ul class="nav navbar-nav navbar-right">
 				<li class="scrubs <?php if ($section == "scrubs") { echo "on"; } ?>"><a href="scrubs.php">Scrubs</a></li>
 				<li class="contact <?php if ($section == "contact") { echo "on"; } ?>"><a href="contact.php">Contact</a></li>
-				<!-- <li class="cart"><a target="paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_cart&amp;business=Q6NFNPFRBWR8S&amp;display=1">Shopping Cart</a></li> -->
+
 			</ul>
-
 		</div>
-
+	</div>
 	</div>
 
+		 	<div class="wrapper">
 	<div id="content">
