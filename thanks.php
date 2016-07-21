02e11edda5f0b375f59cb8e90b72f84b2743a7ef
deetriplett <deetriplett@gmail.com>
2016-07-20 19:24:56 -0700

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,17 @@
+<?php
+
+$pageTitle = "Thank You";
+$section = null;
+include("inc/header.php");
+?>
+ <div class= "container-fluid">
+   <div class="section page">
+     <h1> Thank You </h1>
+     <p> Thanks for the email. We will get back to you shortly!</p>
+
+     <p> In the meantime, check out our delectable sugar <a href = "scrubs.php">scrubs</a> ! </p>
+</div>
+</div>
+
+<?php
+include("inc/footer.php");
