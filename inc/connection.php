5f7209a94b3d7b7a60c213ee92a4493d92caa83e
deetriplett <deetriplett@gmail.com>
2016-07-20 19:22:34 -0700

    Update connection.php

--- inc/connection.php
+++ inc/connection.php
@@ -1,23 +1,20 @@
 <?php
 //Connect to the database
-$servername= "localhost";
 
 try {
-  $db = new PDO('mysql:host= $servername; dbname=database.db');
+  $db = new PDO("mysql:host=localhost;dbname=eatmesugar;port=3306", "user", "password");
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connection successful!";
+  $db->exec("SET NAMES 'utf8'");
 } catch (Exception $e) {
   echo "Unable to connect";
-  //echo $e->getMessage();
   exit;
 }
 
 try {
-  results = $db->query("SELECT * FROM products");
-  echo "Got the scrubs from the pantry!";
+  $results = $db->query("SELECT * FROM products");
 } catch (Exception $e) {
   echo "Unable to get Pantry door opened";
   exit;
 }
 
-// $PDO = new PDO("mysql:dbname=dbname;host=localhost");`
+$products = $results->fetchAll(PDO::FETCH_ASSOC);
