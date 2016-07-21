4daf5952548132ebd2af1cb2c627a35dc6210451
deetriplett <deetriplett@gmail.com>
2016-07-21 09:28:35 -0700

    Cleaned up code

--- scrub.php
+++ scrub.php
@@ -1,5 +1,6 @@
-<?php include("inc/products.php");
-
+<?php 
+include("inc/products.php");
+include("inc/header.php");
 if (isset($_GET["id"])) {
 	$product_id = $_GET["id"];
 	if (isset($products[$product_id])) {
@@ -13,32 +14,30 @@ if (!isset($product)) {
 
 $section = "scrubs";
 $pageTitle = $product["name"];
-include("inc/header.php"); ?>
+?>
 
 <div class="container-fluid">
-		<div class="section page">
-				<div class="breadcrumb"><a href="scrubs.php">Sugar Scrubs</a> &gt; <?php echo $product["name"]; ?></div>
+	<div class="section page">
+		<div class="breadcrumb"><a href="scrubs.php">Sugar Scrubs</a> &gt; <?php echo $product["name"]; ?></div>
 
-			<div class="container-fluid">
-				<div class="col-md-4">
-				<div class="scrub-picture img-responsive">
-					<span>
-						<img src="<?php echo $product["img"]; ?>" class="img-responsive" alt="<?php echo $product["name"]; ?>">
-					</span>
-				</div>
-				</div>
+	<div class="container-fluid">
+		<div class="col-md-4">
+			<div class="scrub-picture img-responsive">
+				<span>
+				<img src="<?php echo $product["img"]; ?>" class="img-responsive" alt="<?php echo $product["name"]; ?>">
+				</span>
+			</div><!--End scrub-pic class-->
+		</div><!--End col-md class-->
 
-
-				<div class="col-md-6">
-				<div class="scrub-details">
-					<h1><span class="price">$<?php echo $product["price"]; ?></span> <?php echo $product["name"]; ?></h1>
-					<p> <?php echo $product["description"]; ?> </p>
-				</div>
-				</div>
-				<div class="col-md-2"></div>
-		</div>
-	</div>
-
-		</div>
+		<div class="col-md-6">
+			<div class="scrub-details">
+				<h1><span class="price">$<?php echo $product["price"]; ?></span> <?php echo $product["name"]; ?></h1>
+				<p> <?php echo $product["description"]; ?> </p>
+			</div><!--End scrub-details class-->
+		</div><!--End col-md class-->
+		<div class="col-md-2"><!--Formatting only--></div>
+	</div><!--End container-->
+	</div><!--End section-->
+</div>
 
 <?php include("inc/footer.php"); ?>
