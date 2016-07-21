be28fed50ecd9fecc92e71ee87b1fcf2baf2159c
deetriplett <deetriplett@gmail.com>
2016-07-21 09:20:35 -0700

    Cleaned up code

--- scrubs.php
+++ scrubs.php
@@ -1,23 +1,20 @@
-<?php include("inc/products.php"); ?><?php
+<?php 
+include("inc/products.php"); 
 $pageTitle = "Full Inventory of Sugar Scrub";
 $section = "scrubs";
 include('inc/header.php'); ?>
 
 <div class="container-fluid">
-		<div class="section scrubs page">
-			<div class="wrapper">
+	<div class="section scrubs page">
+	<div class="wrapper">
+		<h1>Welcome to the Pantry</h1>
+			<ul class="products">
+			<?php foreach($products as $product_id => $product) {
+				echo get_list_view_html($product_id,$product);
+				}?>
+			</ul>
 
-				<h1>Welcome to the Pantry</h1>
-
-				<ul class="products">
-					<?php foreach($products as $product_id => $product) {
-							echo get_list_view_html($product_id,$product);
-						}
-					?>
-				</ul>
-
-			</div>
-
-		</div>
-</div>
+	</div><!--End wrapper-->
+	</div><!--End section-->
+</div><!--End container-->
 <?php include('inc/footer.php') ?>
