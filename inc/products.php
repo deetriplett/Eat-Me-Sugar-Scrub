74e55d141dd5d503ccff3c0f44014f07a27a508d
deetriplett <deetriplett@gmail.com>
2016-07-20 19:21:48 -0700

    Querying the db

--- inc/products.php
+++ inc/products.php
@@ -1,15 +1,58 @@
 <?php
-include("data.php");
+function get_products_recent() {
+  $recent = array();
+  $all = get_products_all();
 
-  //    $sql = 'SELECT * FROM products';
-  //    $products = $db->prepare($sql);
-  //    $products->execute();
-  //    foreach ( $products as $product) {
-  //       $name = $product["name"];
-  //       $desc = $product["description"];
-  //       $price = $product["price"];
-  //       $img = $product["img_path"];
-  //    echo $product;
+  $total_products = count($all);
+  $position = 0;
+
+  foreach($all as $product) {
+    $position = $position + 1;
+    if ($total_products - $postion < 4) {
+      $recent[] = $product;
+    }
+  }
+  return $recent;
+}
+//include("data.php");
+
+    //  $sql = 'SELECT * FROM products';
+    //  $products = $db->prepare($sql);
+    //  $products->execute();
+    //  foreach ( $products as $product) {
+    //     $name = $product["name"];
+    //     $desc = $product["description"];
+    //     $price = $product["price"];
+    //     $img = $product["img_path"];
+    //  echo $product;
+function get_products_all() {
+       try {
+         $db = new PDO("mysql:host=localhost;dbname=eatmesugar;port=3306", "user", "password");
+         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+         $db->exec("SET NAMES 'utf8'");
+       } catch (Exception $e) {
+         echo "Unable to connect";
+         exit;
+       }
+
+       try {
+         $results = $db->query("SELECT * FROM products");
+       } catch (Exception $e) {
+         echo "Unable to get Pantry door opened";
+         exit;
+       }
+
+       $products = $results->fetchAll(PDO::FETCH_ASSOC);
+       return $products;
+}
+
+
+
+
+
+
+
+
 
 function get_list_view_html($product_id, $product) {
 
