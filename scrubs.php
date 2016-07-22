984d763a03341ad628140563d113b92fc083e17f
deetriplett <deetriplett@gmail.com>
2016-07-22 06:35:34 -0700

    Update scrubs.php

--- scrubs.php
+++ scrubs.php
@@ -1,22 +1,24 @@
-<?php 
-include("inc/products.php"); 
+<?php include("inc/products.php"); ?>
+<?php
 $pageTitle = "Full Inventory of Sugar Scrub";
 $section = "scrubs";
 include('inc/header.php'); ?>
 
 <div class="container-fluid">
-	<div class="section scrubs page">
-	<div class="wrapper">
-		<h1>Welcome to the Pantry</h1>
-			<ul class="products">
-			<?php foreach($products as $product_id => $product) {
-				echo get_list_view_html($product_id,$product);
-				}?>
-			</ul>
+		<div class="section scrubs page">
+			<div class="wrapper">
 
-	</div><!--End wrapper-->
-	</div><!--End section-->
-</div><!--End container-->
-<div class="container-fluid">
-<?php include('inc/footer.php') ?>
+				<h1>Welcome to the Pantry</h1>
+
+				<ul class="products">
+					<?php foreach($products as $product_id => $product) {
+							echo get_list_view_html($product_id,$product);
+						}
+					?>
+				</ul>
+
+			</div>
+
+		</div>
 </div>
+<?php include('inc/footer.php') ?>
