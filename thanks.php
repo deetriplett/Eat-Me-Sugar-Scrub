0559442deeddb9dbd401f8d51dd557e646e48c89
deetriplett <deetriplett@gmail.com>
2016-07-22 06:18:43 -0700

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,16 +1,17 @@
 <?php
-include("inc/header.php");
 $pageTitle = "Thank You";
 $section = null;
+include("inc/header.php");
 ?>
+
  <div class= "container-fluid">
    <div class="section page">
      <h1> Thank You </h1>
      <p> Thanks for the email. We will get back to you shortly!</p>
-     <p> In the meantime, check out our delectable sugar <a href = "scrubs.php">scrubs</a> ! </p>
-   </div><!--End section-->
-</div><!--End container-->
 
-<div class="container-fluid">
-<?php include('inc/footer.php') ?>
+     <p> In the meantime, check out our delectable sugar <a href = "scrubs.php">scrubs</a> ! </p>
 </div>
+</div>
+
+<?php
+include("inc/footer.php");
