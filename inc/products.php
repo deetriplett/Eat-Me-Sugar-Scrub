4cfd390c63c29b06a229988a3bd7a65f4cbcf131
deetriplett <deetriplett@gmail.com>
2016-07-22 06:48:06 -0700

    Update products.php

--- inc/products.php
+++ inc/products.php
@@ -1,58 +1,15 @@
 <?php
-function get_products_recent() {
-  $recent = array();
-  $all = get_products_all();
+include("inc/data.php");
 
-  $total_products = count($all);
-  $position = 0;
-
-  foreach($all as $product) {
-    $position = $position + 1;
-    if ($total_products - $postion < 4) {
-      $recent[] = $product;
-    }
-  }
-  return $recent;
-}
-//include("data.php");
-
-    //  $sql = 'SELECT * FROM products';
-    //  $products = $db->prepare($sql);
-    //  $products->execute();
-    //  foreach ( $products as $product) {
-    //     $name = $product["name"];
-    //     $desc = $product["description"];
-    //     $price = $product["price"];
-    //     $img = $product["img_path"];
-    //  echo $product;
-function get_products_all() {
-       try {
-         $db = new PDO("mysql:host=localhost;dbname=eatmesugar;port=3306", "user", "password");
-         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-         $db->exec("SET NAMES 'utf8'");
-       } catch (Exception $e) {
-         echo "Unable to connect";
-         exit;
-       }
-
-       try {
-         $results = $db->query("SELECT * FROM products");
-       } catch (Exception $e) {
-         echo "Unable to get Pantry door opened";
-         exit;
-       }
-
-       $products = $results->fetchAll(PDO::FETCH_ASSOC);
-       return $products;
-}
-
-
-
-
-
-
-
-
+  //    $sql = 'SELECT * FROM products';
+  //    $products = $db->prepare($sql);
+  //    $products->execute();
+  //    foreach ( $products as $product) {
+  //       $name = $product["name"];
+  //       $desc = $product["description"];
+  //       $price = $product["price"];
+  //       $img = $product["img_path"];
+  //    echo $product;
 
 function get_list_view_html($product_id, $product) {
 
@@ -60,7 +17,7 @@ function get_list_view_html($product_id, $product) {
 
     $output = $output . "<li>";
     $output = $output . '<a href="scrub.php?id=' . $product_id . '">';
-    $output = $output . '<img src="' . $product["img"] . '" alt="' . $product["name"] . '" class="img-responsive">';
+    $output = $output . '<img src="' . $product["img_path"] . '" alt="' . $product["name"] . '" class="img-responsive">';
     $output = $output . "<p>View Details</p>";
     $output = $output . "</a>";
     $output = $output . "</li>";
